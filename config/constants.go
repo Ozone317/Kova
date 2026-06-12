7d989de5662d00a6ec7c32af1f0ac0e16e13f275
Daksh <dakshkant8@gmail.com>
2026-06-12 12:52:22 +0530

    feat: add approximate lru eviction policy

--- config/constants.go
+++ config/constants.go
@@ -5,6 +5,6 @@ const (
 	HOST                    = "127.0.0.1"
 	MAX_KEYS                = 1000
 	AOF_FILE                = "./kova-dump.aof"
-	EVICTION_POLICY         = "allkeys-random"
+	EVICTION_POLICY         = "allkeys-lru"
 	ALL_KEYS_EVICTION_RATIO = 0.4
 )
--- core/eviction.go
+++ core/eviction.go
@@ -30,6 +30,42 @@ func getCurrentClock() uint32 {
 	return uint32(time.Now().Unix()) & 0x00FFFFFF // shaving off the first 8 bits to keep the time in 24 bits as Redis does
 }
 
+func getIdleTime(lastAccessedAt uint32) uint32 {
+	c := getCurrentClock()
+
+	if c >= lastAccessedAt {
+		return c - lastAccessedAt
+	}
+
+	return (0x00FFFFFF - lastAccessedAt) + c
+}
+
+func populateEvictionPool() {
+	sampleSize := 5
+	for k := range store {
+		// push the key in the pool
+		// reduce sample size
+		// break if sample size reaches 0
+		ePool.Push(k, store[k].lastAccessedTimeMS)
+		sampleSize--
+		if sampleSize == 0 {
+			break
+		}
+	}
+}
+
+func evictAllkeysLRU() {
+	populateEvictionPool()
+	evictCount := int16(config.ALL_KEYS_EVICTION_RATIO * float64(config.MAX_KEYS))
+	for i := 0; i < int(evictCount) && len(ePool.pool) > 0; i++ {
+		item := ePool.Pop()
+		if item == nil {
+			return
+		}
+		Del([]string{item.key})
+	}
+}
+
 // TODO: Make the eviction strategy configurable
 // TODO: Support multiple eviction strategies
 func evict() {
@@ -38,5 +74,7 @@ func evict() {
 		evictFirst()
 	case "allkeys-random":
 		evictAllkeysRandom()
+	case "allkeys-lru":
+		evictAllkeysLRU()
 	}
 }
